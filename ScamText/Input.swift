fa397bbce9ae678dd24a351ab4a49425969225fd
Anvita <anvigollu@gmail.com>
2023-08-16 13:13:03 -0400

    Fixed the back issue and reduced redundancy

--- ScamText/Input.swift
+++ ScamText/Input.swift
@@ -36,14 +36,14 @@ struct Input: View {
                             .cornerRadius(30)
                             .font(.custom("Arial-Bold", size: 30))
                     }
-                    NavigationLink(destination: ContentView()) {
+                    /*NavigationLink(destination: ContentView()) {
                         Text("Go Back")
                             .frame(width: 200, height: 50, alignment: .center)
                             .background(Color(red: 0.96, green: 0.83, blue:0.37))
                             .foregroundColor(Color(red: 0.10, green: 0.39, blue: 0.49))
                             .cornerRadius(30)
                             .font(.custom("Arial-Bold", size: 30))
-                    }
+                    }*/
                     
                     Spacer()
                     NavigationLink(destination: Help()) {
--- ScamText/help.swift
+++ ScamText/help.swift
@@ -40,14 +40,14 @@ struct Help: View {
                     
                     
 
-                    NavigationLink(destination: ContentView()) {
+                    /*NavigationLink(destination: ContentView()) {
                         Text("Go Back")
                             .frame(width: 200, height: 50, alignment: .center)
                             .background(Color(red: 0.96, green: 0.83, blue:0.37))
                             .foregroundColor(Color(red: 0.10, green: 0.39, blue: 0.49))
                             .cornerRadius(30)
                             .font(.custom("Arial-Bold", size: 30))
-                    }
+                    }*/
                 }
                 
                 .padding()
